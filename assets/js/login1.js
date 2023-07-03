ff52e8739cbb7766de21ae358278625098573f8f
Nguyen Truong Giang <giangliken@gmail.com>
2023-07-03 22:20:26 +0700

    first commit

---
+++ assets/js/login1.js
@@ -0,0 +1,123 @@
+const loginForm = document.querySelector('.login-form');
+    const hiddenContent = document.querySelector('.hidden-content');
+    const logoutBtn = document.querySelector('#logout-btn');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const username = loginForm.querySelector('input[name="uname"]').value; 
+        const password = loginForm.querySelector('input[name="psw"]').value;
+
+        
+        if (username === 'truonggiang' && password === 'truonggiang0761') {
+            alert('Xin chào Trường Giang. Cảm ơn bạn đã đăng nhập với tư cách là Trưởng Nhóm và Quản Trị. Để tiếp tục hãy bấm "OK" ');
+            loginForm.style.display = 'none';
+            hiddenContent.style.display = 'block';
+            
+        } 
+        else
+        if (username === 'tiendong' && password === 'tiendong0704') {
+            alert('Xin chào Tiến Đồng. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'trungtin' && password === 'trungtin3265') {
+            alert('Xin chào Trung Tín. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'manhphi' && password === 'manhphi2336') {
+            alert('Xin chào Mạnh Phi. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'thanhba' && password === 'thanhba4506') {
+            alert('Xin chào Thanh Bá. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'minhtien' && password === 'minhtien3211') {
+            alert('Xin chào Minh Tiến. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'tantai' && password === 'tantai2832') {
+            alert('Xin chào Tấn Tài. Cảm ơn bạn đã đăng nhập với tư cách là Phó Nhóm. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'quangvinh' && password === 'quangvinh7797') {
+            alert('Xin chào Quang Vinh. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'thieny' && password === 'thieny3808') {
+            alert('Xin chào Thiên Ý. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'truchien' && password === 'truchien0900') {
+            alert('Xin chào Trúc Hiền. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else
+        if (username === 'aingan' && password === 'aingan2052') {
+            alert('Xin chào Ái Ngân. Cảm ơn bạn đã đăng nhập với tư cách là thành viên. Để tiếp tục hãy bấm "OK" ');
+            
+            loginForm.style.display = 'none';
+            
+            hiddenContent.style.display = 'block';
+            
+        }
+        else {
+            
+            alert('Tên người dùng hoặc mật khẩu không chính xác!');
+            alert('Nếu gặp lỗi hãy liên hệ với quản trị viên!');
+            
+        }
+    });
+
+    
+    logoutBtn.addEventListener('click', function() {
+        
+        loginForm.style.display = 'block';
+        
+        hiddenContent.style.display = 'none';
+        
+    });
---
+++ assets/js/login2.js
@@ -0,0 +1,51 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault()
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    if (username === "04072004" && password === "04072004") {
+        window.location.href = "https://docs.google.com/document/d/1B-aka7Ku4pFx7eZT_oGo1h1UMJ4nUHI6_bZ8niADzBo/edit?usp=sharing"
+    } 
+    else 
+    if (username === "06052004" && password === "06052004") {
+        window.location.href = "https://docs.google.com/document/d/1ncTx2BISaoBvxQxaSu9BSKzVt3g0JX3U4-a-DhXSNws/edit?usp=sharing"
+    }
+    else 
+    if (username === "28022004" && password === "28022004") {
+        window.location.href = "https://docs.google.com/document/d/170Ry5zA3sSOyu6lOsGk1kIrcG_Bauy_e1tyoBug2QxE/edit?usp=sharing"
+    }
+    else 
+    if (username === "21012004" && password === "21012004") {
+        window.location.href = "https://docs.google.com/document/d/1jU0M1PPbR-b5hMyBGOmGmRCsjNO6YbXrr15AeA2rXRM/edit?usp=sharing"
+    }
+    else
+    if (username === "17022004" && password === "17022004") {
+        window.location.href = "https://docs.google.com/document/d/1aD0iCbzOjMpN-a06tPSFd86T9LW17DOU0F7mVjH-VHc/edit?usp=sharing"
+    }
+    else
+    if (username === "28102001" && password === "28102001") {
+        window.location.href = "https://docs.google.com/document/d/1FggDMj8j3-O36lw6svBpbtxqY3M2e4htRGjYmEAcvZo/edit?usp=sharing"
+    }
+    else
+    if (username === "28042004" && password === "28042004") {
+        window.location.href = "https://docs.google.com/document/d/1y6-PTkEYUfUq49MihazUZeW79OasqaAGHFy_FwG2r2c/edit?usp=sharing"
+    }
+    else
+    if (username === "09032000" && password === "09032000") {
+        window.location.href =" https://docs.google.com/document/d/11R74idRpf0tWYwN9ET7k7BeO-IvyrDIHS9bqOriIuok/edit?usp=sharing"
+    }
+    else
+    if (username === "12082076" && password === "12082076") {
+        window.location.href ="https://docs.google.com/document/d/1pgs_lWkjYg2hFXw9-Svn0mWuh1xKhYfdttZOFY1NzEs/edit?usp=sharing "
+    }
+    else
+    if (username === "14025648" && password === "14025648") {
+        window.location.href =" https://docs.google.com/document/d/1Z413pcI0YdhuCrnepgA9_oeVrvfZgGkvofGsQa9nuow/edit?usp=sharing"
+    }
+    else
+    if (username === "15069652" && password === "15069652") {
+        window.location.href =" https://docs.google.com/document/d/1xyx1W8nv0X2mJhSWT6scXJFWcmUlop6m5EmCbC87NnI/edit?usp=sharing"
+    }
+    else {
+        alert("Mã bảo mật không chính xác!")
+    }
+})
