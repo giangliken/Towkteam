a79df2d37885c4576311e58c7ba739daa5fb352b
Trường Giang <96034649+giangliken@users.noreply.github.com>
2023-07-07 20:30:04 +0700

    Update login2.js

--- assets/js/login2.js
+++ assets/js/login2.js
@@ -2,48 +2,72 @@ document.getElementById("loginForm").addEventListener("submit", function(event){
     event.preventDefault()
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
-    if (username === "04072004" && password === "04072004") {
+    if (username === "04072004" && password === "04072004") {   //Truong Giang
         window.location.href = "https://docs.google.com/document/d/1B-aka7Ku4pFx7eZT_oGo1h1UMJ4nUHI6_bZ8niADzBo/edit?usp=sharing"
     } 
     else 
-    if (username === "06052004" && password === "06052004") {
+    if (username === "06052004" && password === "06052004") {   //Tien Dong
         window.location.href = "https://docs.google.com/document/d/1ncTx2BISaoBvxQxaSu9BSKzVt3g0JX3U4-a-DhXSNws/edit?usp=sharing"
     }
     else 
-    if (username === "28022004" && password === "28022004") {
+    if (username === "28022004" && password === "28022004") {   //Trung Tin
         window.location.href = "https://docs.google.com/document/d/170Ry5zA3sSOyu6lOsGk1kIrcG_Bauy_e1tyoBug2QxE/edit?usp=sharing"
     }
     else 
-    if (username === "21012004" && password === "21012004") {
+    if (username === "21012004" && password === "21012004") {   //Manh Phi
         window.location.href = "https://docs.google.com/document/d/1jU0M1PPbR-b5hMyBGOmGmRCsjNO6YbXrr15AeA2rXRM/edit?usp=sharing"
     }
     else
-    if (username === "17022004" && password === "17122004") {
+    if (username === "17022004" && password === "17122004") {   //Thanh Ba
         window.location.href = "https://docs.google.com/document/d/1aD0iCbzOjMpN-a06tPSFd86T9LW17DOU0F7mVjH-VHc/edit?usp=sharing"
     }
     else
-    if (username === "28102001" && password === "28102001") {
+    if (username === "28102001" && password === "28102001") {   //Minh Tien
         window.location.href = "https://docs.google.com/document/d/1FggDMj8j3-O36lw6svBpbtxqY3M2e4htRGjYmEAcvZo/edit?usp=sharing"
     }
     else
-    if (username === "28042004" && password === "28042004") {
+    if (username === "28042004" && password === "28042004") {   //Tan Tai
         window.location.href = "https://docs.google.com/document/d/1y6-PTkEYUfUq49MihazUZeW79OasqaAGHFy_FwG2r2c/edit?usp=sharing"
     }
     else
-    if (username === "09032000" && password === "09032000") {
+    if (username === "09032000" && password === "09032000") {   //Quang Vinh
         window.location.href =" https://docs.google.com/document/d/11R74idRpf0tWYwN9ET7k7BeO-IvyrDIHS9bqOriIuok/edit?usp=sharing"
     }
     else
-    if (username === "12082076" && password === "12082076") {
+    if (username === "12082076" && password === "12082076") {   //Thien Y
         window.location.href ="https://docs.google.com/document/d/1pgs_lWkjYg2hFXw9-Svn0mWuh1xKhYfdttZOFY1NzEs/edit?usp=sharing "
     }
     else
-    if (username === "14025648" && password === "14025648") {
+    if (username === "14025648" && password === "14025648") {   //Truc Hien
         window.location.href =" https://docs.google.com/document/d/1Z413pcI0YdhuCrnepgA9_oeVrvfZgGkvofGsQa9nuow/edit?usp=sharing"
     }
     else
-    if (username === "15069652" && password === "15069652") {
+    if (username === "15069652" && password === "15069652") {   //Ai Ngan
         window.location.href =" https://docs.google.com/document/d/1xyx1W8nv0X2mJhSWT6scXJFWcmUlop6m5EmCbC87NnI/edit?usp=sharing"
+    }
+    else
+    if (username === "070509" && password === "070509") {   //Gia Huy
+        window.location.href ="https://docs.google.com/document/d/131qCopCaLxcOHgAJB3LHxl1WhkL4V0sM7ROwYJdZ7u0/edit?usp=sharing"
+    }
+    else
+    if (username === "115679" && password === "115679") {   //Khanh Duy
+        window.location.href ="https://docs.google.com/document/d/1CkDvKoQum_z_AYSvKffthvLvv7byc11OTQ2wjbTbEjs/edit?usp=sharing"
+    }
+    else
+    if (username === "115600" && password === "115600") {   //Phuoc Duy
+        window.location.href ="https://docs.google.com/document/d/18VSSuOKBrgaumk5k9ZZRUGHplO6wPpI7b5jlagmb32g/edit?usp=sharing"
+    }
+    else
+    if (username === "005544" && password === "005544") {   //Minh Khang
+        window.location.href ="https://docs.google.com/document/d/1yCIOX7YzLzoL88KpxVV7vL49TGeJPH5RVTT064Qz7JY/edit?usp=sharing"
+    }
+    else
+    if (username === "059874" && password === "059874") {   //Minh Quân
+        window.location.href ="https://docs.google.com/document/d/17hoEC-fgw4yWr8RmtgL_HNIw96oXTe48CDxbv_Cl-dA/edit?usp=sharing"
+    }
+    else
+    if (username === "556699" && password === "556699") {   //huu Nha
+        window.location.href ="https://docs.google.com/document/d/1iNOu7ruAPgjWjVle2bl5hGI9lrM3voLh5aeU0K3V72I/edit?usp=sharing"
     }
     else {
         alert("Mã bảo mật không chính xác!")
